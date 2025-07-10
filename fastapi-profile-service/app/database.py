8ad3ed3dd8b1fcade2f20b511116fcbc1e8199f5
AshishDAC <ashishn@cdac.in>
2025-07-10 15:13:01 +0530

    redeployed4

--- fastapi-profile-service/app/database.py
+++ fastapi-profile-service/app/database.py
@@ -2,14 +2,25 @@
 import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from pymongo.errors import ConnectionFailure
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
-DB_NAME = os.getenv("DB_NAME")
+MONGO_URI = os.getenv("MONGO_URL")
+DB_NAME = os.getenv("DB_NAME","profile_db")
 
-client = MongoClient(MONGO_URI)
-db = client[DB_NAME]
+if not MONGO_URI:
+    raise ValueError("MONGO_URI environment variable not set")
+
+try:
+    client = MongoClient(MONGO_URI)
+    # Test the connection
+    client.admin.command('ping')
+    print("✅ Connected to MongoDB")
+    db = client[DB_NAME]
+except ConnectionFailure as e:
+    print("❌ MongoDB connection failed:", str(e))
+    raise
 
 profile_collection = db.get_collection("profiles")
 
