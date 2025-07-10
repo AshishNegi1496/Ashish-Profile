00f29ecaa0cd4d2cbd635f1c9c97668c7877f8da
AshishDAC <ashishn@cdac.in>
2025-07-10 10:33:55 +0530

    3rd time cors edit

--- ashish-profile/src/app/config.ts
+++ ashish-profile/src/app/config.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL_PROJECTS = "http://10.208.10.157:8081";
 
 // export const API_BASE_URL_PROFILE = "http://10.208.10.157:8000";
-export const API_BASE_URL_PROFILE = "https://ashishnegi.up.railway.app/";
+export const API_BASE_URL_PROFILE = "https://ashishnegi.up.railway.app";
